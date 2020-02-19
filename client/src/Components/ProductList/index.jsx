e9a618c15566736ae118569479990fb07115a1d6
Gabke <gabx.2010@gmail.com>
2020-02-19 20:57:24 -0300

    Added components. Moved Serv and Utils to src

---
+++ client/src/Components/ProductList/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProductList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (<></>);
+    }
+}
+ 
+export default ProductList;
